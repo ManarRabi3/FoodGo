5bc0f224c5f5e75c330a75ca0ff97dfd9213a67d
DELL <mr2079@fayoum.edu.eg>
2025-11-17 20:10:40 +0200

    home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  static const String routName='HomeScreen';
+  static const String routName = 'HomeScreen';
+
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("FoodGo"),
 
+     // backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.red,
+        shape: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(25),
+          borderSide: BorderSide(color: Colors.transparent),
+        ),
+
+        child: Icon(Icons.add, color: Colors.white, size: 35),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.red,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items:const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined), label: "",),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: ""),
+        ],
+      ),
+      body: Column(
+          children:const[
+            SizedBox(height: 60),
+            Text("FoodGo")]),
     );
   }
 }
