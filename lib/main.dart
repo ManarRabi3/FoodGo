975555512a221d46806a5946b4cf079b1c805600
DELL <mr2079@fayoum.edu.eg>
2025-11-25 19:10:06 +0200

    favorite tap

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        PaymentScreen.routeName: (context) =>  const PaymentScreen(),
+       // PaymentScreen.routeName: (context) =>  const PaymentScreen(),
       },
       home: const SplashScreen(),
     );
